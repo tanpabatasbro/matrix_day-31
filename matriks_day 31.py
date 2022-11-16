3048f576468241152f8a0bcc6dc23dc5c6fffa97
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-11-16 22:02:58 +0800

    Add files via upload

---
+++ matriks_day 31.py
@@ -0,0 +1,24 @@
+print(10*"=","MATRIKS",10*"=")
+A = [[5, 0],
+     [2, 6]]
+print("A=",A)
+    
+B = [[5, 0, 8],
+     [2, 6, 7],
+     [1, 3, 4]]
+print("B = ",B)
+
+C = [[1, 4, 5, 12],
+     [-5, 8, 9, 0],
+     [-6, 7, 11, 19]]
+print("C =",C)
+
+print(10*"=","MATRIKS A",10*"=")
+for baris in A:
+    print(baris)
+print(10*"=","MATRIKS B",10*"=")
+for baris in B:
+    print(baris)
+print(10*"=","MATRIKS C",10*"=")
+for baris in C:
+    print(baris)
